d3ea1d61ed64bd9844bdadb990e201783fdf0575
Jhoana Chicaiza <68032185+jhoana-chicaiza@users.noreply.github.com>
2024-08-17 11:51:01 -0500

    Update

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,15 @@
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 class HelloWorld(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"C:\Users\joana\Desktop\chrome-win64\chrome.exe")
-        driver = self.driver
-        driver.implicitly_wait(10)
+        #service = Service(executable_path=r"C:\Users\joana\Desktop\chrome-win64\chrome-win64\chrome.exe")
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+    
     
     def test_hello_world(self):
         driver = self.driver
@@ -17,4 +19,4 @@ class HelloWorld(unittest.TestCase):
         self.driver.quit()
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='report', report_name='HelloWorldReport'))
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reporte', report_name='hello-world-report'))
